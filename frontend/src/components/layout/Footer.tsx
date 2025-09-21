616bf4ef61bbd8dcde70ca4531d8bf7fb21f8710
bharat.kumawat <bharat.kumawat@kadellabs.com>
2025-09-21 14:50:45 +0530

    fix layout and ts issues

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -32,10 +32,10 @@ const Footer = () => {
   };
 
   const socialLinks = [
-    { name: 'Facebook', icon: Facebook, href: '#' },
-    { name: 'Twitter', icon: Twitter, href: '#' },
-    { name: 'Instagram', icon: Instagram, href: '#' },
-    { name: 'YouTube', icon: Youtube, href: '#' },
+    { name: 'Facebook', icon: Facebook, href: 'https://www.facebook.com/people/Pedal-India/61557798781166/#' },
+    { name: 'Twitter', icon: Twitter, href: 'https://www.facebook.com/people/Pedal-India/61557798781166/#' },
+    { name: 'Instagram', icon: Instagram, href: 'https://www.instagram.com/jatin.bhagwani/?hl=en' },
+    { name: 'YouTube', icon: Youtube, href: 'https://www.youtube.com/@jatinbhagwani8259?app=desktop' },
   ];
 
   return (
